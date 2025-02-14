cc74c6e5b5e8839c3cecec4b46fa23b135672f45
Zeynel KOZAK <zeynell.kozak@outlook.com>
2025-02-14 17:21:29 +0300

    feat: created alert component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import type { App } from 'vue';
+import { Alert } from './components';
 
 export default {
-  install(app: App) {}
+  install(app: App) {
+    app.component('LibAlert', Alert);
+  }
 };
 
+export {
+  Alert
+};
---
+++ src/stories/Alert.stories.ts
@@ -0,0 +1,64 @@
+import { Alert } from '@/components';
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+const meta: Meta<typeof Alert> = {
+  component: Alert,
+  title: 'Messaging/Alert',
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['solid', 'outline']
+    },
+    color: {
+      control: 'select',
+      options: ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'dark']
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Alert>;
+
+export const Default: Story = {
+  args: {
+    variant: 'solid',
+    color: 'primary',
+    title: 'Default Alert',
+    text: 'This is a default alert',
+  },
+  render: (args) => ({
+    components: { Alert },
+    setup() {
+      return { args };
+    },
+    template: `
+    <Alert v-bind="args"></Alert>
+    `,
+  })
+};
+
+export const Slots: Story = {
+  args: {
+    variant: 'solid',
+    color: 'primary',
+    title: 'Default Alert',
+    text: 'This is a default alert',
+  },
+  render: (args) => ({
+    components: { Alert },
+    setup() {
+      return { args };
+    },
+    template: `
+    <Alert v-bind="args">
+      <template #title>
+        Default Alert
+      </template>
+      <template #text>
+        This is a default alert
+      </template>
+    </Alert>
+    `,
+  })
+};
