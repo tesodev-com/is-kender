a5675ebde3a51847f5ee684974cc96a9fbc02b18
gorkemgunay <60298426+gorkemgunay@users.noreply.github.com>
2025-02-19 11:28:13 +0300

    feat: add export

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import type { App } from 'vue';
+import { useResizeObserver } from '@/composables/useResizeObserver';
 
 export default {
   install(app: App) {}
 };
 
+export { useResizeObserver };
