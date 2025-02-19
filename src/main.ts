269eebdf300e7b917ca8b0c6c12d4b7bf1fbd133
gorkemgunay <60298426+gorkemgunay@users.noreply.github.com>
2025-02-19 11:29:01 +0300

    feat: add export

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import type { App } from 'vue';
+import { useIntersectionObserver } from '@/composables/useIntersectionObserver';
 
 export default {
   install(app: App) {}
 };
 
+export { useIntersectionObserver };
