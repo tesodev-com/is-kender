5a5e7623fe5413f8a88007bba9bf27369fcb488a
gorkemgunay <60298426+gorkemgunay@users.noreply.github.com>
2025-02-20 12:13:13 +0300

    feat: remove exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
 export { default as Toggle } from './Toggle/Toggle.vue';
-export { default as ProgressBar } from './ProgressBar.vue';
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import type { App } from 'vue';
-import { Toggle, ProgressBar } from './components';
+import { Toggle } from './components';
 
 export default {
   install(app: App) {
     app.component('LibToggle', Toggle);
-    app.component('LibProgressBar', ProgressBar);
   }
 };
 
 export {
   Toggle,
-  ProgressBar
 };
