b808c871ebe41264cd5f1991283c52eef1c8c784
gorkemgunay <60298426+gorkemgunay@users.noreply.github.com>
2025-02-20 12:14:54 +0300

    feat: remove exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
 export { default as Toggle } from './Toggle/Toggle.vue';
-export { default as Text } from './Text/Text.vue';
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import type { App } from 'vue';
-import { Toggle, Text } from './components';
+import { Toggle } from './components';
 
 export default {
   install(app: App) {
     app.component('LibToggle', Toggle);
-    app.component('LibText', Text);
   }
 };
 
 export {
   Toggle,
-  Text
 };
