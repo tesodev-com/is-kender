24242585ddadd831ffdd909d55548a9889fd919f
gorkemgunay <60298426+gorkemgunay@users.noreply.github.com>
2025-02-20 12:18:04 +0300

    feat: remove exports

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import type { App } from 'vue';
 import { Toggle } from './components';
-import { useIsMobile } from '@/composable/useIsMobile';
 
 export default {
   install(app: App) {
@@ -10,5 +9,4 @@ export default {
 
 export {
   Toggle,
-  useIsMobile
 };
