15ee512bb5e831815527d0759c17bbbcd25c8d63
gorkemgunay <60298426+gorkemgunay@users.noreply.github.com>
2025-02-20 12:16:19 +0300

    feat: remove exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
 export { default as Toggle } from './Toggle/Toggle.vue';
-export { default as Badge } from './Badge/Badge.vue';
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import type { App } from 'vue';
-import { Toggle, Badge } from './components';
+import { Toggle } from './components';
 
 export default {
   install(app: App) {
     app.component('LibToggle', Toggle);
-    app.component('LibBadge', Badge);
   }
 };
 
 export {
   Toggle,
-  Badge
 };
