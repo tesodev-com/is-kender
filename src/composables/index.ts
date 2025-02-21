3dc8d1da8ab3571e3ac19e092022fa935328fbc0
Zeynel KOZAK <zeynell.kozak@outlook.com>
2025-02-21 17:59:07 +0300

    feat: updated build methods

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,3 +1,8 @@
-export * from './useToast';
-export * from './useTheme';
+export * from './useDebounce';
 export * from './useEventBus';
+export * from './useIntersectionObserver';
+export * from './useIsMobile';
+export * from './useResizeObserver';
+export * from './useTheme';
+export * from './useToast';
+
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
-import { useEventBus, useToast } from '@/composables';
+import { useToast } from '@/composables';
 import eventBus from '@/utils/eventBus';
 import type { App } from 'vue';
-import { Alert, Breadcrumb, Button, Col, Container, FileUpload, Row, Skeleton, Svg, Toast, Toggle } from './components';
+import * as components from './components';
 
 export default {
   install(app: App) {
     app.config.globalProperties.$eventBus = eventBus;
     app.config.globalProperties.$toast = useToast();
-    app.component('LibToggle', Toggle);
-    app.component('LibSkeleton', Skeleton);
-    app.component('LibAlert', Alert);
-    app.component('LibButton', Button);
-    app.component('LibFileUpload', FileUpload);
-    app.component('LibToast', Toast);
-    app.component('LibContainer', Container);
-    app.component('LibRow', Row);
-    app.component('LibCol', Col);
-    app.component('LibBreadcrumb', Breadcrumb);
-    app.component('LibSvg', Svg);
+    Object.entries(components).forEach(([name, component]) => {
+      app.component(name, component);
+    });
   },
 };
 
-export { Alert, Breadcrumb, Button, Col, Container, eventBus, FileUpload, Row, Skeleton, Svg, Toast, Toggle, useEventBus, useToast };
+export * from './components';
+export * from './composables';
+
