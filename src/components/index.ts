56becdf9ff8ab85472b3228e64eaa5e5274187ce
Zeynel KOZAK <zeynel.kozak@tesodev.com>
2025-03-18 23:08:14 +0300

    feat(Pagination): add Pagination component to package and export from index

--- src/components/index.ts
+++ src/components/index.ts
@@ -11,6 +11,7 @@ export { default as Drawer } from 'library/Drawer';
 export { default as FileUpload } from 'library/FileUpload';
 export { default as Input } from 'library/Input';
 export { default as Link } from 'library/Link';
+export { default as Pagination } from 'library/Pagination';
 export { default as ProgressBar } from 'library/ProgressBar';
 export { default as RadioButton } from 'library/RadioButton';
 export { default as Row } from 'library/Row';
