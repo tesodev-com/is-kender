2049e7b2617c3e7b2c7d8d61cf98f19f4d39e1f6
Zeynel KOZAK <zeynel.kozak@tesodev.com>
2025-03-21 18:25:57 +0300

    chore: clean up Storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
-import * as path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -7,18 +6,6 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
-  },
-  viteFinal: config => {
-    if (config.resolve) {
-      config.resolve.alias = {
-        '@': path.resolve(__dirname, '../src'),
-        'library-components': path.resolve(__dirname, '../src/components'),
-        'library-composables': path.resolve(__dirname, '../src/composables'),
-        'library-directives': path.resolve(__dirname, '../src/directives'),
-        'library-utils': path.resolve(__dirname, '../src/utils'),
-      };
-    }
-    return config;
   },
   staticDirs: ['../public'],
 };
